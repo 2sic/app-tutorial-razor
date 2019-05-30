cbd9f6af23746c4e501cea9c2f70a5bb8a1504d7
ijungleboy <daniel.mettler@2sic.com>
2019-05-30 12:28:56 +0200

    wip

--- api/VerifiedController.cs
+++ api/VerifiedController.cs
@@ -1,8 +1,10 @@
 // the using statements tell the compiler what you need
 // the first one is needed so you can write [HttpGet] and [AllowAnonymous]
 // The second one is needed so the compiler can find the SxcApiController
+// The final one allows us to verify the AntiForgeryToken
 using System.Web.Http;
 using ToSic.SexyContent.WebApi;
+using DotNetNuke.Web.Api;
 
 // Tell the server that all commands can be accessed without a login
 // But requires the user to be on your website
