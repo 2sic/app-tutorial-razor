0d06347dfbcb0130c9b5141697929863e73b3f6c
ijungleboy <daniel.mettler@2sic.com>
2019-05-30 15:07:44 +0200

    webapi tutorials enhanced

--- live/api/DemoController.cs
+++ live/api/DemoController.cs
@@ -1,6 +1,9 @@
 using System.Web.Http;
 using ToSic.SexyContent.WebApi;
+using DotNetNuke.Web.Api;
 
+[AllowAnonymous]
+[ValidateAntiForgeryToken]
 public class DemoController : SxcApiController
 {
 	[HttpGet]
@@ -9,5 +12,4 @@ public class DemoController : SxcApiController
 	{
 		return "Hello from the live controller";
 	}
-
 }
