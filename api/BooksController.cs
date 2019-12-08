ee21b967e2be3a22e8b4ad0fba7a2672754d2af4
ijungleboy <daniel.mettler@2sic.com>
2019-12-08 22:03:19 +0100

    api examples with App.Data

--- api/BooksController.cs
+++ api/BooksController.cs
@@ -1,33 +1,39 @@
-using System.Web.Http;		// this enables [HttpGet] and [AllowAnonymous]
-using System.Linq;
-using DotNetNuke.Web.Api;	// this is to verify the AntiForgeryToken
+using System.Linq;        // this enables .Select(x => ...)
+using System.Web.Http;    // this enables [HttpGet] and [AllowAnonymous]
+using DotNetNuke.Web.Api; // this is to verify the AntiForgeryToken
 using Dynlist = System.Collections.Generic.IEnumerable<dynamic>;
 
-[AllowAnonymous]			// define that all commands can be accessed without a login
-[ValidateAntiForgeryToken]	// protects the API from users not on your site (CSRF protection)
+[AllowAnonymous]            // define that all commands can be accessed without a login
+[ValidateAntiForgeryToken]  // protects the API from users not on your site (CSRF protection)
 // Inherit from ToSic...ApiController to get features like App, Data or Dnn - see https://r.2sxc.org/CustomWebApi
 public class BooksController : ToSic.Sxc.Dnn.ApiController
 {
+  [HttpGet]
+  public dynamic Persons()
+  {
+    // we could do: return App.Data["Persons"];
+    // but the raw data is difficult to use, so we'll improve it
 
-	[HttpGet]				// [HttpGet] says we're listening to GET requests
-	public dynamic Persons()
-	{
-		return App.Data["Persons"];
-	}
+    // instead we'll create nice objects for our use
+    return AsList(App.Data["Persons"])
+      .Select(p => new {
+        Id = p.EntityId,
+        p.FirstName,
+        p.LastName,
+        Picture = p.Mugshot,
+      });
+  }
 
-	[HttpGet]				// [HttpGet] says we're listening to GET requests
-	public dynamic Books()
-	{
-		return App.Data["Books"];
-	}
-
-	[HttpGet]				// [HttpGet] says we're listening to GET requests
-	public dynamic BookAuthors()
-	{
-		var firstBook = AsList(App.Data["Books"])
-			.First();
-		return (firstBook.Authors as Dynlist)
-			.Select(a => a.FirstName + " " + a.LastName);
-	}
-
+  [HttpGet]
+  public dynamic Books()
+  {
+    return AsList(App.Data["Books"])
+      .Select(b => new {
+        Id = b.EntityId,
+        b.Title,
+        b.Cover,
+        Awards = AsList(b.Awards as object)
+          .Select(a => a.EntityTitle)
+      });
+  }
 }
