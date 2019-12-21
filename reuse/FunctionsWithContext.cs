af81828b0f09bb4b7c297259efce16f99b772b16
ijungleboy <daniel.mettler@2sic.com>
2019-12-21 09:26:51 +0100

    updated example with DynamicCode

--- reuse/FunctionsWithContext.cs
+++ reuse/FunctionsWithContext.cs
@@ -3,8 +3,7 @@
 // - This inherits from ToSic.Sxc.Code.WithDnnContext
 //   which will automatically provide the common objects like App, Dnn etc.
 //   from the current context to use in your code
-using ToSic.Sxc.Code;
-public class FunctionsWithContext: WithDnnContext {
+public class FunctionsWithContext: ToSic.Sxc.Dnn.DynamicCode {
 
   public string QrPath(string link) {
         // path to qr-code generator
