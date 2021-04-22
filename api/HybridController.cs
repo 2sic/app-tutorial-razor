4714a58107778e6ecb84d7f7919097cbb893c880
ijungleboy <daniel.mettler@2sic.com>
2021-04-22 14:32:23 +0200

    wip Hybrid

--- api/HybridController.cs
+++ api/HybridController.cs
@@ -1,24 +1,33 @@
-// Add namespaces to enable [HttpGet] and [AllowAnonymous]
-// In Oqtane it's on a different .net namespace than in Dnn
+// Add namespaces to enable security in Oqtane & DNN despite the differences
 #if OQTANE
-using Microsoft.AspNetCore.Authorization; // for [Authorize]
-using Microsoft.AspNetCore.Mvc; // for [AllowAnonymous]
+using Microsoft.AspNetCore.Authorization; // .net core [AllowAnonymous] & [Authorize]
+using Microsoft.AspNetCore.Mvc;           // .net core [HttpGet] / [HttpPost] etc.
+using Oqtane.Shared;        // Oqtane role names
 #else
-using System.Web.Http; // for [AllowAnonymous] 
+using System.Web.Http;      // .net 4.5 [AllowAnonymous] / [HttpGet]
+using DotNetNuke.Web.Api;   // [DnnModuleAuthorize]
+using DotNetNuke.Security;  // SecurityAccessLevel.Xyz
 #endif
 
 // Todo: verification token?
 
 // All commands can be accessed without security checks
-[AllowAnonymous]
 public class HybridController : ToSic.Custom.Api12
 {
-  [HttpGet] public string Hello()
+  [AllowAnonymous]  // Works in Oqtane and DNN
+  [HttpGet]         // Works in Oqtane and DNN
+  public string Hello()
   {
     return "Hello from the basic controller in /api";
   }
 
-  [HttpGet] public int Square(int number)
+  #if OQTANE
+  [Authorize(Roles = RoleNames.Everyone)]
+  #else
+  [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Anonymous)]
+  #endif
+  [HttpGet]
+  public int Square(int number)
   {
     return number * number;
   }
