853a215ba5741a38648af959e7c5a4334c35ffb9
ijungleboy <daniel.mettler@2sic.com>
2021-04-24 15:00:29 +0200

    wip v12 / oqtane

--- api/HybridController.cs
+++ api/HybridController.cs
@@ -5,13 +5,14 @@ using Microsoft.AspNetCore.Mvc;           // .net core [HttpGet] / [HttpPost] et
 using Oqtane.Shared;        // Oqtane role names
 #else
 using System.Web.Http;      // .net 4.5 [AllowAnonymous] / [HttpGet]
-using DotNetNuke.Web.Api;   // [DnnModuleAuthorize]
+using DotNetNuke.Web.Api;   // [DnnModuleAuthorize] & [ValidateAntiForgeryToken]
 using DotNetNuke.Security;  // SecurityAccessLevel.Xyz
 #endif
 
-// Todo: verification token?
-
-// All commands can be accessed without security checks
+#if OQTANE
+[Produces("application/json")]  // Ensures that strings are JSON and have quotes around them in .net 5
+#endif
+[ValidateAntiForgeryToken]      // This has the same name, but comes from a different namespace
 public class HybridController : ToSic.Custom.Api12
 {
   [AllowAnonymous]  // Works in Oqtane and DNN
