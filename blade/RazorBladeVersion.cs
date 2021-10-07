89fe933ee67ce6bc7097ab06d33cf900323e0750
ijungleboy <daniel.mettler@2sic.com>
2021-10-07 23:52:41 +0200

    wip

--- blade/RazorBladeVersion.cs
+++ blade/RazorBladeVersion.cs
@@ -8,17 +8,17 @@ public class RazorBladeVersion: Custom.Hybrid.Code12
 
   public Version GetRazorBladeVersion() {
     var div = ToSic.Razor.Blade.Tag.Div();
-    var version = Assembly.GetAssembly(div.GetType()).GetName().Version; //.ToString(4);
+    var version = Assembly.GetAssembly(div.GetType()).GetName().Version;
     return version;
   }
 
-  public double VersionDouble() {
+  public float VersionDouble() {
     var verInfo = GetRazorBladeVersion();
     if(verInfo == null) return 0;
 
     var major = verInfo.Major;
     var minor = verInfo.Minor;
-    return Convert.ToDouble(major + "." + minor.ToString("D2"));
+    return Convert.ToFloat(major + "." + minor.ToString("D2"));
   }
 
   public string VersionInfo(double version, double expected) {
