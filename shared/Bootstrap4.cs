43183038593f9ec22b7afc6a0e863f6bd4866ebd
ijungleboy <daniel.mettler@2sic.com>
2021-10-07 16:01:21 +0200

    minor linting etc.

--- shared/Bootstrap4.cs
+++ shared/Bootstrap4.cs
@@ -1,5 +1,4 @@
 using ToSic.Razor.Blade;
-using Connect.Koi;
 
 public class Bootstrap4 : Custom.Hybrid.Code12
 {
@@ -7,15 +6,15 @@ public class Bootstrap4 : Custom.Hybrid.Code12
   // this solves both the cases where its unknown, or another framework
   public void EnsureBootstrap4()
   {
-    var pageCss = GetService<ICss>();
-    if(!pageCss.Is("bs4")) {
+    var pageCss = GetService<Connect.Koi.ICss>();
+    if(pageCss.IsUnknown) {
       GetService<ToSic.Sxc.Web.IPageService>().Activate("Bootstrap4");
     }
   }
 
   // show warning for admin if koi.json is missing
   public dynamic WarnAboutMissingOrUnknownBootstrap() {
-    var pageCss = GetService<ICss>();
+    var pageCss = GetService<Connect.Koi.ICss>();
     if (pageCss.IsUnknown && CmsContext.User.IsSiteAdmin) {
       return Tag.Div().Class("dnnFormMessage dnnFormWarning").Wrap(
         Connect.Koi.Messages.CssInformationMissing,
