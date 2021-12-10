53d2902786d05301fe5536bc8cf2235aae73a30f
ijungleboy <daniel.mettler@2sic.com>
2021-12-10 13:29:40 +0100

    minor docs

--- turn-on/_100-js-example.js
+++ turn-on/_100-js-example.js
@@ -1,3 +1,4 @@
+// The init function which should be called
 function init() {
   // example element gets found in the DOM
   const exampleElement = document.querySelector("#turn-on-example")
@@ -5,5 +6,12 @@ function init() {
   exampleElement.innerText = "turnOn has been executed. 😉";
 }
 
-window.razorTutorial = window.razorTutorial || {};
-window.razorTutorial.init = init;
+// For demo purpose, wait 3 seconds before we add the object and Init
+// This should simulate slow loading of a JavaScript
+
+setTimeout(() => {
+  // Generate a new object if none exists yet (best practice)
+  window.turnOnTutorial100 = window.turnOnTutorial100 || {};
+  window.turnOnTutorial100.init = window.turnOnTutorial100.init || init;
+}, 3000);
+
--- turn-on/_110-js-example.js
+++ turn-on/_110-js-example.js
@@ -1,10 +1,20 @@
-// data gets passed as a single object, so we need to deconstruct it.
-function init({word1, word2}) {
+// Simple init with Data object
+function init(data) {
+  // example element gets found in the DOM
+  const exampleElement = document.querySelector("#turn-on-example")
+  // parameters get displayed in the DOM
+  exampleElement.innerText = `turnOn has passed the parameters: ${data.word1} ${data.word2}!`;
+}
+
+// This is a more modern JS feature to deconstruct parameters
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+function demoDeconstruct({word1, word2}) {
   // example element gets found in the DOM
   const exampleElement = document.querySelector("#turn-on-example")
   // parameters get displayed in the DOM
   exampleElement.innerText = `turnOn has passed the parameters: ${word1} ${word2}!`;
 }
 
-window.razorTutorial = window.razorTutorial || {};
-window.razorTutorial.init = init;
+const rt = window.razorTutorial = window.razorTutorial || {};
+rt.init = rt.init || init;
+rt.deconstructed = rt.deconstructed || demoDeconstruct;
