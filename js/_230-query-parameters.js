8282e8c25dd282a984b4b89d370ce38f72b3af75
Maxim Hüsler <maxim.huesler@2sic.com>
2022-01-06 10:43:32 +0100

    License, improvements img and js tutorial

--- js/_230-query-parameters.js
+++ js/_230-query-parameters.js
@@ -1,66 +1,64 @@
-window.queryParameters = {
+
+window.tutQueryParameters = {
   querySvc: null,
-  // Object with query parameters
-  poetQueryParameters: {
-    Name: '',
-    Poems: -1
-  },
-  // This is a more modern JS feature to deconstruct parameters
-  // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
   init: function({ moduleId }) {
     // Create a $2sxc object using the current Module Id
     const sxc = $2sxc(moduleId);
+    this.querySvc = sxc.query('AuthorsWithBooks');
 
-    // Get the data source using .data('xy')
-    this.querySvc = sxc.data('Poets');
+    // Attach click-handlers to button
+    document.getElementById(`mod-${moduleId}-object`).onclick = () => this.queryWithObjectParams();
+    document.getElementById(`mod-${moduleId}-string`).onclick = () => this.queryWithStringParams();
+    document.getElementById(`mod-${moduleId}-stream-params`).onclick = () => this.queryStreamWithParams();
+    document.getElementById(`mod-${moduleId}-streams-params`).onclick = () => this.queryStreamsWithParams();
+  },
 
-    // Read data from the backend data source with the .getAll() query
-    this.querySvc.getAll().then((poets) => {
-      // pass poets to displayPoets
-      this.displayPoets(poets);
+  queryWithObjectParams: function() {
+    const queryParameters = {
+      authorId: 20068,
+      authorPageResults: 2,
+      currentAuthorPage: 1
+    };
+
+    this.querySvc.getAll(queryParameters).then(data => {
+      console.log("Get query with passing url parameters as object", data);
+      alert('Got all - see console for details. \n \n' + JSON.stringify(data, null, 2));
     });
   },
 
-  // Display example data in table
-  displayPoets: function(poets) {
-    document.querySelector('#example-content > tbody').innerHTML = "";
-    Array.prototype.forEach.call(poets.reverse(), (poet, poetIndex) => {
-      // Make sure only 3 elements are shown
-      if (poetIndex >= 3) return
-      
-      let tr = document.createElement('tr')
-      
-      this.addField(tr, poet.Name);
-      this.addField(tr, new Date(poet.BirthDate).toLocaleDateString());
-      this.addField(tr, poet.Poems);
+  queryWithStringParams: function() {
+    // For passing parameters as a string it's important to use the default query string structure (key=value seperated by a &)
+    // See: https://en.wikipedia.org/wiki/Query_string#Structure
+    const stringWithParameters = 'authorId=20068&authorPageResults=2&currentAuthorPage=1';
 
-      document.querySelector('#example-content > tbody').appendChild(tr)
+    this.querySvc.getAll(stringWithParameters).then(data => {
+      console.log("Get query with passing url parameters as string", data);
+      alert('Got all - see console for details. \n \n' + JSON.stringify(data, null, 2));
     });
   },
 
-  queryByParameters: function() {
-    // Retrieve set Query Parameters from DOM
-    this.poetQueryParameters.Name = document.getElementById("param-name").value;
-    this.poetQueryParameters.Poems = document.getElementById("param-poems").value;
+  queryStreamWithParams: function() {
+    const queryParameters = {
+      AuthorId: 20068
+    };
 
-    // Build Query string
-    const queryString = new URLSearchParams(this.poetQueryParameters).toString();
-
-    const isQueryString = document.getElementById("is-querystring").value
-    if (isQueryString) console.log(`Query string: ${queryString}`)
-
-    // TODO 2mh: ask 2dm how query using parameters works
-    // Read data from the backend data source with the .getAll() query and additional parameters
-    // Note: Parameters can be passed as a string or object
-    this.querySvc.getAll(isQueryString ? queryString : this.poetQueryParameters).then((poets) => {
-      // Pass poets to displayPoets
-      this.displayPoets(poets);
+    // Ask 2dm why this doesnt work
+    this.querySvc.getStream('Current', queryParameters).then(data => {
+      console.log("Get Query stream with passing url parameter", data);
+      alert('Got all - see console for details. \n \n' + JSON.stringify(data, null, 2));
     });
   },
 
-  addField: function(tr, text) {
-    let td = document.createElement('td')
-    td.innerText = text
-    tr.appendChild(td)
-  }
+  queryStreamsWithParams: function() {
+    const queryParameters = {
+      authorId: 20068,
+      authorPageResults: 2,
+      currentAuthorPage: 1
+    };
+
+    this.querySvc.getStreams('Current,AuthorsByPage', queryParameters).then(data => {
+      console.log("Get query streams with passing url parameter", data);
+      alert('Got all - see console for details. \n \n' + JSON.stringify(data, null, 2));
+    });
+  },
 }
