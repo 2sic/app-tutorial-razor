add85bf3aaf0f0d476a61ebe28e4577adf993e47
ijungleboy <daniel.mettler@2sic.com>
2022-07-05 23:16:37 +0200

    wip

--- formulas/Helpers.cs
+++ formulas/Helpers.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using ToSic.Razor.Blade;
 public class Helpers : Custom.Hybrid.Code14 {
   public dynamic GetFormulas(string contentType, string fieldName) {
-    var contentItem = AsList(App.Data[contentType]).FirstOrDefault();
-    var contentItemType = AsEntity(contentItem).Type;
-    var fieldType = AsList(contentItemType.Attributes as object).Where(a => a.Name == fieldName).FirstOrDefault();
+    var contentItemType = App.AppState.GetContentType(contentType);
+    var fieldType = contentItemType.Attributes
+      .Where(a => a.Name == fieldName)
+      .FirstOrDefault();
     
     var attributeMd = AsList(fieldType.Metadata.OfType("@All") as object).FirstOrDefault();
     return AsList(attributeMd.Formulas as object);
