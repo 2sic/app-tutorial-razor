9bbaa068a91104f7a41dbc5f3220cca4e1699876
getBlup <daia.robbi@gmail.com>
2022-07-27 11:44:08 +0200

    added the cmscontext and changed the ui tutorial

--- formulas/Helpers.cs
+++ formulas/Helpers.cs
@@ -18,18 +18,7 @@ public class Helpers : Custom.Hybrid.Code14 {
     return "svg:PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iNDgiIHdpZHRoPSI0OCI+PHBhdGggZD0iTTEyIDQwVjM2Ljg1TDI1Ljc1IDI0TDEyIDExLjE1VjhIMzZWMTNIMTkuOEwzMS41NSAyNEwxOS44IDM1SDM2VjQwWiIvPjwvc3ZnPg==";
   }
 
-  public string[] DemoRules(string contentType, string more = null) {
-    var newButton = "+new&icon=" + Icon() + "?contentType=" + contentType;
-    if (Text.Has(more)) {
-      newButton += "&" + more;
-    }
-    return new [] {
-      "toolbar=empty",
-      newButton
-    };
-  }
-
   public dynamic DemoToolbar(string contentType, string more = null) {
-    return Edit.Toolbar(toolbar: DemoRules(contentType, more));
+    return Kit.Toolbar.Empty().New(contentType, ui:"icon=" + Icon() + (Text.Has(more) ? more : ""));
   }
 }
