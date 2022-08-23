e5b15da17105509625a11c24c131d8f81f75868e
ijungleboy <daniel.mettler@2sic.com>
2022-08-23 23:53:15 +0200

    improve tut: demo-toolbars now always show

--- ui/shared.cs
+++ ui/shared.cs
@@ -1,8 +1,9 @@
+using ToSic.Sxc.Demo;
+
 // Important notes: 
 // - This class should have the same name as the file it's in
 public class Shared : Custom.Hybrid.Code14 
 {
-
   public void EnableEditForAnonymous(dynamic Edit) {
     // Special command to ensure that the toolbars appear, even if they are won't work.
     // This is not an official API, and may change any time.
@@ -11,5 +12,21 @@ public class Shared : Custom.Hybrid.Code14
 
     // This will tell the edit-UI that we need the JS features
     Kit.Page.Activate("2sxc.JsCore", "2sxc.JsCms", "2sxc.Toolbars");
+  }
+
+  // Special internal API which will make the toolbars always show
+  // even without hover. This is an internal API for demos only. 
+  // Must be added after the intro section of this file, as that can also create many toolbar 
+  // which shouldn't be affected
+  public string AutoShowAllToolbarsStart() {
+    Kit.Toolbar.ActivateDemoMode(ui: "show=always");
+    return "";// Return empty string so this command can be used inline
+  }
+
+  // Special internal API which will make the toolbars always show
+  // even without hover. This is an internal API for demos only. 
+  public string AutoShowAllToolbarsEnd() {
+    Kit.Toolbar.ActivateDemoMode(ui: null);
+    return "";
   }
 }
