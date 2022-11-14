3bd1b2ecd21d887f2800e485414a7dcfa9a6ae44
ijungleboy <daniel.mettler@2sic.com>
2022-11-15 00:35:28 +0100

    experimental

---
+++ shared/SourceCode.cs
@@ -0,0 +1,57 @@
+using ToSic.Razor.Blade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class SourceCode: Custom.Hybrid.Code14
+{
+  const int LineHeightPx = 20;
+  const int BufferHeightPx = 20; // for footer scrollbar which often appears
+
+  public string SourceTrim(string source) {
+    // optimize to remove leading or trailing (but not in the middle)
+    var lines = (source ?? "").Split(Environment.NewLine.ToCharArray()).ToList();
+    var result = DropLeadingEmpty(lines);
+    result.Reverse();
+    result = DropLeadingEmpty(result);
+    result.Reverse();
+    return string.Join("\n", result);
+  }
+
+  private List<string> DropLeadingEmpty(List<string> lines) {
+    var dropEmpty = true;
+    return lines.Select(l => {
+      if (!dropEmpty) return l;
+      if (l.Trim() == "") return null;
+      dropEmpty = false;
+      return l;
+    })
+    .Where(l => l != null)
+    .ToList();
+  }
+
+  // Auto-calculate Size
+  public int Size(object sizeObj, string source) {
+    var size = Kit.Convert.ToInt(sizeObj, fallback: -1);
+    if (size == -1) {
+      var sourceLines = source.Split('\n').Length;
+      size = sourceLines * LineHeightPx + BufferHeightPx;
+    }
+
+    if (size < LineHeightPx) size = 600;
+    return size;
+  }
+
+  // Determine the ace9 language of the file
+  public string FindAce3LanguageName(string filePath) {
+    var extension = filePath.Substring(filePath.LastIndexOf('.') + 1);
+    switch (extension)
+    {
+      case "cs": return "csharp";
+      case "js": return "javascript";
+      case "json": return "json";
+      default: return "razor";
+    }
+  }
+}
