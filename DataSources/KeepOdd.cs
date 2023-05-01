e0e799f03c79ee9b0a131c361db0f990694d442a
ijungleboy <daniel.mettler@2sic.com>
2023-05-01 16:10:52 +0200

    wip custom datasource keepOdd

---
+++ DataSources/KeepOdd.cs
@@ -0,0 +1,16 @@
+using System.Linq;
+
+public class KeepOdd : Custom.DataSource.DataSource16
+{
+  public KeepOdd(MyServices services) : base(services)
+  {
+    ProvideOut(() => {
+      // Make sure we have an In stream - otherwise return an error
+      var inStream = TryGetIn();
+      if (inStream == null) return Error.TryGetInFailed();
+
+      // Return only odd items
+      return inStream.Where(e => e.EntityId % 2 == 1);
+    });
+  }
+}
