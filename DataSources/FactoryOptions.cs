94068639d515ce3010d1428b5309868d2b58e54f
ijungleboy <daniel.mettler@2sic.com>
2023-05-01 20:09:40 +0200

    app state sync and more samples

---
+++ DataSources/FactoryOptions.cs
@@ -0,0 +1,21 @@
+using System.Linq;            // This is for the Enumerable.Range()
+using ToSic.Eav.Data.Build;   // This is for the FactoryOptions
+
+public class FactoryOptions : Custom.DataSource.DataSource16
+{
+  public FactoryOptions(MyServices services) : base(services)
+  {
+    ProvideOut(() =>
+      // For demo, create a few of these items
+      Enumerable.Range(0, 4).Select(i => new {
+        // We don't provide an ID, which will result in an id=0 which will auto-id
+        // Id = i,
+        Guid = System.Guid.NewGuid(),
+        // No more Title, now it's Greeting - which wouldn't auto-map to EntityTitle
+        Greeting = "Greeting from FactoryOptions",
+      }),
+      // Set various options for how the factory will generate the Entities
+      options: () => new DataFactoryOptions(typeName: "MyContentType", titleField: "Greeting", idSeed: 1000)
+    );
+  }
+}
