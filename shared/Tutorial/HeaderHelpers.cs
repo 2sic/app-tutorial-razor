1af4b32007a583b515119baf0622b68695f26cf2
iJungleboy <daniel.mettler@2sic.com>
2023-09-23 18:24:30 +0200

    more cleanup

--- shared/Tutorial/HeaderHelpers.cs
+++ shared/Tutorial/HeaderHelpers.cs
@@ -5,7 +5,13 @@ using System.Linq;
 
 public class HeaderHelpers: Custom.Hybrid.CodeTyped
 {
-  public IHtmlTag AddFeatureLogo(string path, string link, int size = 0) {
+  public dynamic AddMainLogo(string path, string link, int size = 0) {
+    var img = Tag.Div(Tag.Img().Src(App.Folder.Url + "/" + path + "?w=75&h=75").Class("img-fluid")).Class("icon-wrapper");
+    if (size != 0) img.Style("height: " + size + "px;" + " width: " + size + "px;");
+    return Tag.A().Href(link).Target("_blank").Wrap(img.Class("float-right ml-3 ms-3 float-end"));
+  }
+
+  public IHtmlTag AddFeatureIconFromSegment(string path, string link, int size = 0) {
     var img = Tag.Div(
         Tag.Img().Src(path + "?w=75&h=75")
           .Class("img-fluid")
