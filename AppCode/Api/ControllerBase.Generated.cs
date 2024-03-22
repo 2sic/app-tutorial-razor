42fa5cd9ef27a59c4d662b2ea9f9e0172ea5fa81
iJungleboy <daniel.mettler@2sic.com>
2024-03-22 17:06:06 +0100

    start generating models/base classes

---
+++ AppCode/Api/ControllerBase.Generated.cs
@@ -0,0 +1,26 @@
+// DO NOT MODIFY THIS FILE - IT IS AUTO-GENERATED
+// All the classes are partial, so you can extend them in a separate file.
+
+// Generator:   WebApiGenerator v17.05.00
+// App/Edition: Tutorial-Razor/
+// User:        2sic Web-Developer
+// When:        2024-03-22 17:04:49Z
+
+using AppCode.Data;
+using ToSic.Sxc.Apps;
+
+namespace AppCode.Api
+{
+  /// <summary>
+  /// Base Class for Services which have a typed App.
+  /// </summary>
+  public abstract partial class ControllerBase: Custom.Hybrid.ApiTyped
+  {
+
+    /// <summary>
+    /// Typed App with typed Settings & Resources
+    /// </summary>
+    public new IAppTyped<AppSettings, AppResources> App => _app ??= Customize.App<AppSettings, AppResources>();
+    private IAppTyped<AppSettings, AppResources> _app;
+  }
+}
