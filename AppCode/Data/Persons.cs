065701c32699e2881155c1ab6f59f44251d4c466
iJungleboy <daniel.mettler@2sic.com>
2024-05-13 18:43:32 +0200

    various strong-typed samples

--- AppCode/Data/Persons.cs
+++ AppCode/Data/Persons.cs
@@ -2,6 +2,11 @@ namespace AppCode.Data
 {
   public partial class Persons
   {
-    // Add your own properties and methods here
+    /// <summary>
+    /// Custom property Presentation - as in these tutorials
+    /// the Person always uses a QuickRefContentPresentation content-type for the presentation
+    /// </summary>
+    public new QuickRefContentPresentation Presentation => _presentation ??= As<QuickRefContentPresentation>(base.Presentation);
+    private QuickRefContentPresentation _presentation;
   }
 }
