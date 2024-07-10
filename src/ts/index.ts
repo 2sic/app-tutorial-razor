dabc27fd62e329a293bb4b1fc60ea6d837f28329
iJungleboy <daniel.mettler@2sic.com>
2024-07-10 13:57:41 +0200

    update ace9 to use latest version and specified base path

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -11,11 +11,22 @@ function initSourceCode({ domAttribute, aceOptions  }: { domAttribute: string, a
   // debug
   // console.log('args', arguments);
 
+  // set base path - in case it's loaded as a module or similar and doesn't know its path
+  // you can always debug this in the browser using commands such as:
+  // ace.config.get('themePath')
+  const acePath = "https://cdn.jsdelivr.net/npm/ace-builds@1.35.2/src-noconflict";
+  ace.config.set('basePath', acePath);
+  ace.config.set('themePath', acePath);
+  ace.config.set('modePath', acePath);
+  // console.log('ace.config', ace.config);
+
   // set theme
   let editor = ace.edit(aceOptions.sourceCodeId, { 
-    useWrapMode: true,
+    wrap: true,
     maxLines: 30
   });
+
+  // console.log('editor', editor, editor.getOptions());
 
   editor.setTheme("ace/theme/sqlserver");
   editor.session.setMode(aceOptions.language);
