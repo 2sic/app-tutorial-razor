b53d2a245287ce933364e5eb54df05d1be175839
paulpietzko <paul.pietzko@icloud.com>
2025-08-28 09:37:12 +0200

    feat: add spectrum color picker and replace pickr-pro with it

---
+++ system/field-string-app-color-picker-spectrum/index.js
@@ -0,0 +1,86 @@
+/*
+  This example shows a plain JS WebComponent which uses Spectrum Vanilla for color picking.
+  Uses Spectrum Vanilla from https://github.com/LeaVerou/spectrum
+  This simple picker has a predefined set of colors and doesn't allow field configuration
+*/
+
+// always use an IIFE to ensure you don't put variables in the window scope
+(() => {
+  const tagName = 'field-string-app-color-picker-spectrum';
+  const spectrumJsCdn = 'https://unpkg.com/spectrum-vanilla/dist/spectrum.min.js';
+  const spectrumCssCdn = 'https://unpkg.com/spectrum-vanilla/dist/spectrum.min.css';
+  const html = `
+    <link rel="stylesheet" href="${spectrumCssCdn}"/>
+    <div class="spectrum-container">
+      <input id="color-picker" type="text" />
+    </div>`;
+
+  class StringColorPicker extends HTMLElement {
+
+    /** connectedCallback() is the standard callback when the component has been attached */
+    connectedCallback() {
+      this.innerHTML = html;
+      this.input = this.querySelector('#color-picker');
+      // Set initial value from connector if exists
+      this.input.value = this.connector.data.value || '';
+      // load Spectrum if not loaded
+      this.connector.loadScript('Spectrum', spectrumJsCdn, () => { this.initSpectrum() });
+    }
+
+    /** disconnectedCallback() is a standard callback for clean-up */
+    disconnectedCallback() {
+      if (this.sp) {
+        this.sp.destroy();
+        this.sp = null;
+      }
+    }
+
+    /** This is called when the JS is loaded from loadScript - so Spectrum is ready */
+    initSpectrum() {
+      this.sp = Spectrum.create(this.input, {
+        showInput: true,
+        showInitial: true,
+        preferredFormat: "hex",
+        showPalette: true,
+        allowEmpty: true, // enables clear functionality
+        palette: [
+          ['#276cb8', '#ff0000', '#00ff00', '#0000ff'],
+          ['#ffffff', '#000000']
+        ],
+        change: color => this.handleChange(),
+        hide: () => this.handleHide()
+      });
+
+      // remember if we're working empty as of now
+      this.cleared = !this.connector.data.value;
+    }
+
+    /** Update the value when color is selected */
+    handleChange() {
+      this.cleared = false;
+      // Spectrum Vanilla updates the input element
+      const value = this.input.value;
+      this.updateIfChanged(value);
+    }
+
+    /** Update the value when picker is closed */
+    handleHide() {
+      // Only update if value actually changed or cleared
+      if (this.cleared) {
+        this.updateIfChanged(null);
+      }
+    }
+
+    /** Only update the value if it really changed, so form isn't dirty if nothing was set */
+    updateIfChanged(value) {
+      var data = this.connector.data;
+      // Debug output, see what's being sent
+      if (data.value === '' && value == null) return;
+      if (data.value === value) return;
+      data.update(value);
+    }
+  }
+
+  // Register this web component - if it hasn't been registered yet
+  if (!customElements.get(tagName)) customElements.define(tagName, StringColorPicker);
+})();
